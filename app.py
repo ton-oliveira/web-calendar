c484d6265dfef9f1332afcf05d953d06a5e4d911
ton-oliveira <87735990+ton-oliveira@users.noreply.github.com>
2021-09-22 19:24:09 -0300

    Handle GET and POST requests from the client

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
+import datetime
+
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, inputs, reqparse
 import sys
 
 app = Flask(__name__)
 api = Api(app)
 
+TODOS = {"message": "The event has been added!",
+         "event": "????",
+         "date": "????"
+         }
 
-# write your code here
+parser = reqparse.RequestParser()
+parser.add_argument(
+    "event",
+    type=str,
+    help="The event name is required!",
+    required=True
+)
+
+parser.add_argument(
+    'date',
+    type=inputs.date,
+    help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
+    required=True
+)
+
+
+class EventAdd(Resource):
+    def post(self):
+        args = parser.parse_args()
+        TODOS['event'] = args['event']
+        TODOS['date'] = str(args['date'].date())
+
+        return TODOS
+
+
 class EventsToday(Resource):
     def get(self):
         return {
@@ -14,6 +44,7 @@ class EventsToday(Resource):
         }
 
 
+api.add_resource(EventAdd, '/event')
 api.add_resource(EventsToday, '/event/today')
 # do not change the way you run the program
 if __name__ == '__main__':
@@ -21,4 +52,4 @@ if __name__ == '__main__':
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
     else:
-        app.run()
+        app.run(debug=True)
